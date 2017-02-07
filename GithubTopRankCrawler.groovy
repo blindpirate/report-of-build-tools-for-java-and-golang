2e502f826b28bf3015287806a1035641b04e1df0
Bo Zhang <zhangbodut@gmail.com>
2017-02-07 18:01:36 +0800

    Add support for multiple language

--- GithubTopRankCrawler.groovy
+++ GithubTopRankCrawler.groovy
@@ -7,27 +7,29 @@ import java.nio.file.Path
 import java.nio.file.Paths
 
 class GithubTopRankCrawler {
-    static String TARGET_URL = 'https://api.github.com/search/repositories?q=stars%3A%3E1+language%3AGo&sort=stars&order=desc&type=Repositories&page=${page}&per_page=100'
+    static String TARGET_URL = 'https://api.github.com/search/repositories?q=stars%3A%3E1+language%3A${language}&sort=stars&order=desc&type=Repositories&page=${page}&per_page=100'
     static String TOP_JSON = 'top.json'
 
     static void main(String[] args) {
-        if (args.length == 0) {
-            println 'a location must be specified!'
+        if (args.length < 2) {
+            println 'lauguage and location must be specified!'
             return
         }
-        Path baseDir = Paths.get(args[0])
-        getTop1000(baseDir).each {
+        String language = args[0]
+        Path baseDir = Paths.get(args[1])
+        getTop1000(language, baseDir).each {
             cloneOne(baseDir, it.full_name, it.clone_url)
         }
     }
 
-    static List getTop1000(Path baseDir) {
+    static List getTop1000(String language, Path baseDir) {
         File topDotJson = baseDir.resolve(TOP_JSON).toFile()
         if (topDotJson.exists()) {
             return new JsonSlurper().parseText(topDotJson.getText())
         } else {
+            String url = TARGET_URL.replace('${language}', language)
             List allItems = (1..10).collect({ it ->
-                String json = new URL(TARGET_URL.replace('${page}', it.toString())).getText()
+                String json = new URL(url.replace('${page}', it.toString())).getText()
                 return new JsonSlurper().parseText(json).items
             }).flatten()
             topDotJson.write(JsonOutput.toJson(allItems))
